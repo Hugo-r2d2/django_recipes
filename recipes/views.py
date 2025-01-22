d7899ea7c06123bfae149099fc1e0c02675c146f
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-01-22 17:20:11 -0400

    config urls

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/home.html')   
